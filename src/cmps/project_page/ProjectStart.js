ade6cc22af920d0a202cdea1a1770208d4d3ce55
titanium25 <idpm7N95tLPLskg>
2021-10-28 09:57:39 +0300

    add project page

---
+++ src/cmps/project_page/ProjectStart.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from "../../assets/images/Under construction.svg";
+
+const ProjectStart = () => {
+    return (
+        <div className="projects">
+            <div className="new--project">
+                <object data={logo} type="image/svg+xml"/>
+            </div>
+
+        </div>
+    );
+};
+
+export default ProjectStart;
---
+++ src/pages/ProjectPage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ProjectStart from "../cmps/project_page/ProjectStart";
+
+const ProjectPage = () => {
+    return (
+        <div>
+
+            <ProjectStart />
+
+        </div>
+    );
+};
+
+export default ProjectPage;
