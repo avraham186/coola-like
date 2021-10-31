270d1dde731a2594a0d96db5f6e7876e70682b02
Gera184 <oogeraoo159@gmail.com>
2021-10-31 23:27:31 +0200

    gera

--- src/App.js
+++ src/App.js
@@ -1,31 +1,28 @@
-import './assets/main.scss';
-import {HashRouter as Router, Route, Switch} from 'react-router-dom'
-import {routes} from './routes';
-import {AppNav} from './cmps/AppNav'
-import {AppFooter} from './cmps/AppFooter'
+import "./assets/main.scss";
+import { HashRouter as Router, Route, Switch } from "react-router-dom";
+import { routes } from "./routes";
+import { AppNav } from "./cmps/AppNav";
+import { AppFooter } from "./cmps/AppFooter";
 
 export function App() {
-    return (
-        <div className="main-layout">
-            <Router>
-                <AppNav/>
-                <Switch>
-                    {
-                        routes
-                            .map(route => {
-                                return (
-                                    <Route
-                                        key={route.path}
-                                        exact
-                                        component={route.component}
-                                        path={route.path}
-                                    />
-                                )
-                            })
-                    }
-                </Switch>
-                <AppFooter/>
-            </Router>
-        </div>
-    );
+  return (
+    <div className="main-layout">
+      <Router>
+        <AppNav />
+        <Switch>
+          {routes.map((route) => {
+            return (
+              <Route
+                key={route.path}
+                exact
+                component={route.component}
+                path={route.path}
+              />
+            );
+          })}
+        </Switch>
+        <AppFooter />
+      </Router>
+    </div>
+  );
 }
