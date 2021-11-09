147bf45320343d0b9896e4118293159360ed2a9c
ruthhubara052768 <r0527680508@gmail.com>
2021-11-09 02:00:57 +0200

    task redux toolkit

--- src/store/entities.js
+++ src/store/entities.js
@@ -1,10 +1,11 @@
-import {combineReducers} from "redux";
+import { combineReducers } from "redux";
 import projectsReducer from './projects';
-import {jobReducer} from "./reducers/jobReducer";
-import {communityHeartReducer} from "./reducers/communityHeartReducer";
-
+import { jobReducer } from "./reducers/jobReducer";
+import { communityHeartReducer } from "./reducers/communityHeartReducer";
+import tasksReducer from './tasks'
 export default combineReducers({
     projects: projectsReducer,
     jobsModule: jobReducer,
+    tasks: tasksReducer,
     communityHeartModule: communityHeartReducer
 })
