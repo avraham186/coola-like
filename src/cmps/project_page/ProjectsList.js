7cda9c4edb13e474c26bd9355781a31b19edd88f
shany.pollack <shany777@gmail.com>
2021-11-09 16:16:47 +0200

    first commit

--- src/cmps/project_page/ProjectsList.js
+++ src/cmps/project_page/ProjectsList.js
@@ -1,51 +1,51 @@
-import React, {useEffect} from 'react';
+import React, { useEffect } from "react";
 import projectsDAL from "../../adapters/TMS/projectsDAL";
-import {loadProjects} from "../../store/projects";
-import {useDispatch, useSelector} from "react-redux";
-import {Paper} from "@material-ui/core";
+import { loadProjects } from "../../store/projects";
+import { useDispatch, useSelector } from "react-redux";
+import { Paper } from "@material-ui/core";
 
 const ProjectsList = () => {
+  const deleteProject = async (id) => await projectsDAL.deleteProject(id);
 
-    const deleteProject = async (id) => await projectsDAL.deleteProject(id);
+  const dispatch = useDispatch();
+  const projects = useSelector((state) => state.entities.projects);
 
-    const dispatch = useDispatch();
-    const projects = useSelector(state => state.entities.projects)
+  useEffect(() => {
+    dispatch(loadProjects());
+    console.log(projects.list);
+  }, []);
 
-    useEffect(() => {
-        dispatch(loadProjects());
-        console.log(projects.list)
-    }, [])
+  return (
+    <div className="projects-table">
 
+      {projects.list.map((v, i) => {
+        return (
 
-    return (
-        <div className="projects-table">
-            {
-                projects.list.map((v, i) => {
-                    return (
-                        <div className="projects-row">
+          <div className="projects-row">
+          <tr>
+          <td>Project name: </td>
 
-                            <Paper elevation={3} className="row-item" >
-                                {v.projectName}
-                            </Paper>
-                            <Paper elevation={3} className="row-item" >
-                                {v.description}
-                            </Paper>
-                            <Paper elevation={3} className="row-item" >
-                                {v.startDate}
-                            </Paper>
-                            <Paper elevation={3} className="row-item" >
-                                {v.endDate}
-                            </Paper>
-                            <Paper elevation={3} className="row-item" >
-                                {v.projectStatus}
-                            </Paper>
-                        </div>
-                    )
-                })
-            }
-        </div>
-
-    )
+          </tr>
+            {/* <Paper elevation={3} className="row-item" > */}
+            {v.projectName}
+            {/* </Paper> */}
+            {/* <Paper elevation={3} className="row-item" > */}
+            {/* {v.description} */}
+            {/* </Paper> */}
+            {/* <Paper elevation={3} className="row-item" > */}
+            {v.startDate}
+            {/* </Paper>
+                            <Paper elevation={3} className="row-item" > */}
+            {v.endDate}
+            {/* </Paper>
+                            <Paper elevation={3} className="row-item" > */}
+            {v.projectStatus}
+            {/* </Paper> */}
+          </div>
+        );
+      })}
+    </div>
+  );
 };
 
 export default ProjectsList;
