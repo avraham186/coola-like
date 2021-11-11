aa47cfcc35820634e4034af730bcda3c6400e95d
ruthhubara052768 <r0527680508@gmail.com>
2021-11-11 16:59:21 +0200

    update and delete project

--- src/pages/ProjectPage.js
+++ src/pages/ProjectPage.js
@@ -1,15 +1,7 @@
 import React, { useState } from 'react';
 import EmptyProjects from "../cmps/project_page/EmptyProjects";
 import ProjectsList from "../cmps/project_page/ProjectsList";
-<<<<<<< HEAD
 import { Button } from "@material-ui/core";
-import { TextField, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, InputLabel, MenuItem, FormControl, Select } from '@mui/material';
-import { DesktopDatePicker, LocalizationProvider } from '@mui/lab'
-import AdapterDateFns from '@mui/lab/AdapterDateFns';
-import { useDispatch, useSelector } from "react-redux";
-import { addProject, loadProjects } from "../store/projects";
-=======
-import {Button} from "@material-ui/core";
 import TextField from '@mui/material/TextField';
 import Dialog from '@mui/material/Dialog';
 import DialogActions from '@mui/material/DialogActions';
@@ -24,7 +16,6 @@ import MenuItem from '@mui/material/MenuItem';
 import FormControl from '@mui/material/FormControl';
 import Select from '@mui/material/Select';
 import SideBarAdmin from '../cmps/project_page/sideBarAdmin/SideBarAdmin';
->>>>>>> main
 
 import { useDispatch } from "react-redux";
 import { addProject } from "../store/projects";
