e614fe2cfa6391a8676e88b7cbc01fbcb2fc85ba
ruthhubara052768 <r0527680508@gmail.com>
2021-11-14 12:33:49 +0200

    login

--- src/routes.js
+++ src/routes.js
@@ -3,23 +3,32 @@ import HomePage from './pages/HomePage'
 import ProjectPage from "./pages/ProjectPage";
 import EventsPage from './pages/EventsPage';
 import NewTask from './cmps/tasks/NewTask'
-
+import Login from './cmps/login/login'
 export const routes = [
+
     {
         path: '/',
         component: HomePage
+    },
+    {
+        path: '/login',
+        component: Login
     },
     {
         path: '/projects',
         component: ProjectPage
     },
     {
-        path: "/projects/task/newTask",
+        path: "/projects/task/new-task/:projectId",
         component: NewTask,
+    },
+    {
+        path: '/events',
+        component: EventsPage
     },
     {
         path: "/projects/task/:projectId",
         component: TaskList,
-    },
+    }
 
 ]
