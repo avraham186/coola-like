614d1ba93173698d9669ded5416454354cf93590
Avraham <avraham186@gmail.com>
2021-11-18 20:04:59 +0200

    jobs page connect with server && fix expand bug

--- src/cmps/home_page/Jobs.jsx
+++ src/cmps/home_page/Jobs.jsx
@@ -1,9 +1,18 @@
-import React from 'react';
-import {Link} from "react-router-dom";
+import React, { useState, useEffect } from 'react';
+import { loadJobs } from '../../store/jobs.js';
+import { Link } from "react-router-dom";
 import ArrowBackIcon from "@mui/icons-material/ArrowBack";
-import JobCard from "./sub_cmps/JobCard.jsx";
+import { JobCard } from "./sub_cmps/JobCard.jsx";
+import { useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
 
 const Jobs = () => {
+    const [expanded, setExpanded] = useState(false);
+    const dispatch = useDispatch()
+    const jobs = useSelector(state => state.entities.jobs)
+    useEffect(() => {
+        dispatch(loadJobs())
+    }, [])
     return (
         <div className="jobs">
             <span className="jobs-title">#לוח_משרות לג'וניורים</span>
@@ -12,14 +21,16 @@ const Jobs = () => {
                 <span className="jobs-sub-title-link">
                     <Link to="/">
                         <span className="jobs-link">לכל הפרטים</span>
-                        <ArrowBackIcon className="sub-title-icon"/>
+                        <ArrowBackIcon className="sub-title-icon" />
                     </Link>
                 </span>
             </div>
             <div className="jobs-cards">
-                <JobCard/>
-                <JobCard/>
-                <JobCard/>
+                {jobs.list.slice(0, 3).map((job) => {
+                    return <div key={job.id}>
+                        <JobCard job={job} />
+                    </div>
+                })}
             </div>
         </div>
     );
