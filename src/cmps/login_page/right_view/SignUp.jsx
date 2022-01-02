953b6f4ed9a23ab372dfdf850aad7ae7cc9f2b4f
GavrielHay <gavriel660@gmail.com>
2022-01-02 21:19:26 +0200

    singUp ready: using fullName param for now. multiselect buttons for interest desinged (need little fix)

--- src/cmps/login_page/right_view/SignUp.jsx
+++ src/cmps/login_page/right_view/SignUp.jsx
@@ -50,73 +50,56 @@ const useStyles = makeStyles((theme) => ({
     icon: {
         left: 10
     },
+   
     
-    
+    interestBox: {
+        flexWrap: "wrap",
+        //gap: "rem(15px)",
+        height: "17px",
+        background: "#FFFFFF",
+        border: "0.5px solid #C4C4C4",
+        borderRadius: "10px",
+        //cursor: "pointer",
+        padding: '10px 10px 10px 10px',
+        color: "#2C2C2C",
+
+        'input:hover ~ &': {
+            background: "rgba(52, 1, 142, 0.3)",
+         },
+         /*
+        "&.active": {
+            flexWrap: "wrap",
+            gap: "rem(15px)",
+            height: "37px",
+            background: (props) => props.background,
+            border: "0.5px solid #C4C4C4",
+            borderRadius: "10px",
+            //cursor: "pointer",
+            padding: '10px 10px 10px 10px',
+            color: "#2C2C2C",
+        },
+        */
+    },
+
+    interestBoxChecked: {
+        flexWrap: "wrap",
+        //gap: "rem(15px)",
+        height: "17px",
+        background: "rgba(52, 1, 142, 0.2)",
+        border: "0.5px solid #C4C4C4",
+        borderRadius: "10px",
+        //cursor: "pointer",
+        padding: '10px 10px 10px 10px',
+        color: "#2C2C2C",
+        'input:hover ~ &': {
+            background: "rgba(52, 1, 142, 0.4)",
+         },
+
+    },
+
 }));
 
 
-const BpIcon = styled('span')(({ theme }) => ({
-    borderRadius: 3,
-    width: 16,
-    height: 16,
-    boxShadow:
-      theme.palette.mode === 'dark'
-        ? '0 0 0 1px rgb(16 22 26 / 40%)'
-        : 'inset 0 0 0 1px rgba(16,22,26,.2), inset 0 -1px 0 rgba(16,22,26,.1)',
-    backgroundColor: theme.palette.mode === 'dark' ? '#394b59' : '#f5f8fa',
-    backgroundImage:
-      theme.palette.mode === 'dark'
-        ? 'linear-gradient(180deg,hsla(0,0%,100%,.05),hsla(0,0%,100%,0))'
-        : 'linear-gradient(180deg,hsla(0,0%,100%,.8),hsla(0,0%,100%,0))',
-    '.Mui-focusVisible &': {
-      outline: '2px auto rgba(19,124,189,.6)',
-      outlineOffset: 2,
-    },
-    'input:hover ~ &': {
-      backgroundColor: theme.palette.mode === 'dark' ? '#30404d' : '#ebf1f5',
-    },
-    'input:disabled ~ &': {
-      boxShadow: 'none',
-      background:
-        theme.palette.mode === 'dark' ? 'rgba(57,75,89,.5)' : 'rgba(206,217,224,.5)',
-    },
-  }));
-  
-  const BpCheckedIcon = styled(BpIcon)({
-    className:"flex align-center justify-center",
-    backgroundColor: '#137cbd',
-    backgroundImage: 'linear-gradient(180deg,hsla(0,0%,100%,.1),hsla(0,0%,100%,0))',
-    '&:before': {
-      display: 'block',
-      width: 16,
-      height: 16,
-      backgroundImage:
-        "url(\"data:image/svg+xml;charset=utf-8,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 16 16'%3E%3Cpath" +
-        " fill-rule='evenodd' clip-rule='evenodd' d='M12 5c-.28 0-.53.11-.71.29L7 9.59l-2.29-2.3a1.003 " +
-        "1.003 0 00-1.42 1.42l3 3c.18.18.43.29.71.29s.53-.11.71-.29l5-5A1.003 1.003 0 0012 5z' fill='%23fff'/%3E%3C/svg%3E\")",
-      content: '""',
-    },
-    'input:hover ~ &': {
-      backgroundColor: '#106ba3',
-    },
-  });
-  
-  // Inspired by blueprintjs
-  function BpCheckbox(props) {
-    return (
-      <Checkbox
-        sx={{
-          '&:hover': { bgcolor: 'transparent' },
-        }}
-        disableRipple
-        color="default"
-        checkedIcon={<BpCheckedIcon />}
-        icon={<BpIcon />}
-        inputProps={{ 'aria-label': 'Checkbox demo' }}
-        {...props}
-      />
-    );
-  }
   
 
 
@@ -130,7 +113,6 @@ const SignUp = () => {
     const dispatch = useDispatch();
 
     const [passVerify, setPassVerify] = useState("");
-
 
     const [interests, setInterests] = useState([]);
 
@@ -193,7 +175,7 @@ const SignUp = () => {
         */
     }
 
-
+ 
 
     return (
         <div className="login--form">
@@ -206,7 +188,7 @@ const SignUp = () => {
 
                 <div className="login-form-inputs">
                     <form onSubmit={handleSubmit(send)}>
-
+        
                         {/*<Inputs inputs={textFieldsArr} />*/}
                         <Input className={classes.inputs} id="fullName" label="שם מלא (אנגלית)" type= 'text' required={true} registerObj={{...register("fullName")}}/>
                         <Input className={classes.inputs} id="Email" label= "אימייל" type= 'email' required={true}  registerObj={{...register("email")}}/>
@@ -216,23 +198,34 @@ const SignUp = () => {
                         <div className="login--form--inputs" style={{ marginTop: 30, textAlign:'right'}}  >
                            בחר תחומי עניין
                         </div>
+
+                        <div className="interest flex column">
+                            <FormControl >
+                                <FormGroup>
+                                <div className="interest-content flex">
+                                {Array.from(interestsModel).map(item=>
+                                    <Checkbox key = {item.key} checked={item.checked} onChange={handleInterestsChange} name={item.name} 
+                                        className={classes.interestBox}
+                                        disableRipple
+                                        color="default"
+                                        checkedIcon={<span className={classes.interestBoxChecked}>
+                                        {item.label}
+                                        </span>}
+                                        icon={<span className={classes.interestBox}>
+                                        {item.label}
+                                        </span>}
+                                        //inputProps={{ "aria-label": 'Checkbox demo' }}
+                                        />
+                                    )
+                                }
+                                </div>
+                                </FormGroup>
+                            </FormControl>
+                        </div>
+
+                        {/*} THE ORIGINAL MUI CHECK-BOX DESIGNE:
                         <FormControl >
                             <FormGroup >
-                           {Array.from(interestsModel).map(item=>
-                                <FormControlLabel
-                                    key = {item.key}
-                                    control={
-                                    <BpCheckbox  checked={item.checked} onChange={handleInterestsChange} name={item.name} />
-                                    }
-                                    label={item.label}
-                                />)
-                            }
-                            </FormGroup>
-                        </FormControl>
-
-
-                        <FormControl >
-                            <FormGroup className="interest-content flex" >
                            {Array.from(interestsModel).map(item=>
                                 <FormControlLabel
                                     key = {item.key}
@@ -244,7 +237,8 @@ const SignUp = () => {
                             }
                             </FormGroup>
                         </FormControl>
-                        
+                        {*/}
+
                         <Button 
                         type="submit" variant="contained" color="primary"
                             className={classes.submit} size='large' 
