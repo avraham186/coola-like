b3e157c23af9b4356e2420ff9b92818f47803670
Avraham <avraham186@gmail.com>
2022-01-04 21:57:36 +0200

    more progress on notefications component press on the toggle change to on/off

--- src/cmps/profile/ProfileNotefications.jsx
+++ src/cmps/profile/ProfileNotefications.jsx
@@ -30,23 +30,23 @@ export const ProfileNotefications = () => {
                     </div>
                     <div className="new-event-on-my-interest flex space-between align-center">
                         <span>קבלת התראות על אירועים חדשים הקשורים לתחום העניין שלי</span>
-                        <div className={`btn-notefications ${setDivClassName('interestEvent')} flex align-center`}>
-                            <span className={`toggle ${setSpanClassName('interestEvent')}`}
-                                onClick={() => handleChange("interestEvent")}></span>
+                        <div className={`btn-notefications ${setDivClassName('interestEvent')} flex align-center`}
+                            onClick={() => handleChange("interestEvent")}>
+                            <span className={`toggle ${setSpanClassName('interestEvent')}`}></span>
                         </div>
                     </div>
                     <div className="new-jobs-on-my-interest flex space-between align-center">
                         <span>קבלת התראות על משרות חדשות הקשורות לתחום העניין שלי</span>
-                        <div className={`btn-notefications ${setDivClassName('jobs')} flex align-center`}>
-                            <span className={`toggle ${setSpanClassName('jobs')}`}
-                                onClick={() => handleChange("jobs")}></span>
+                        <div className={`btn-notefications ${setDivClassName('jobs')} flex align-center`}
+                            onClick={() => handleChange("jobs")}>
+                            <span className={`toggle ${setSpanClassName('jobs')}`}></span>
                         </div>
                     </div>
                     <div className="news flex space-between align-center">
                         <span>קבלת התראות על כתבות חדשות שעולות בטיפים ומידע</span>
-                        <div className={`btn-notefications ${setDivClassName('news')} flex align-center`}>
-                            <span className={`toggle ${setSpanClassName('news')}`}
-                                onClick={() => handleChange("news")}></span>
+                        <div className={`btn-notefications ${setDivClassName('news')} flex align-center`}
+                            onClick={() => handleChange("news")}>
+                            <span className={`toggle ${setSpanClassName('news')}`}></span>
                         </div>
                     </div>
                 </div>
@@ -54,51 +54,51 @@ export const ProfileNotefications = () => {
             {isAdmin() && <div className="admin-notefication">
                 <div className="me-tagged flex space-between align-center">
                     <span>קבלת התראות כשמתייגים אותי במשימה</span>
-                    <div className={`btn-notefications ${setDivClassName('me-tagged')} flex align-center`}>
-                        <span className={`toggle ${setSpanClassName('me-tagged')}`}
-                            onClick={() => handleChange("me-tagged")}></span>
+                    <div className={`btn-notefications ${setDivClassName('me-tagged')} flex align-center`}
+                        onClick={() => handleChange("me-tagged")}>
+                        <span className={`toggle ${setSpanClassName('me-tagged')}`}></span>
                     </div>
                 </div>
                 <div className="my-missions-updates flex space-between align-center">
                     <span>קבלת התראות על עדכונים במשימות שלי</span>
-                    <div className={`btn-notefications ${setDivClassName('my-missions-updates')} flex align-center`}>
-                        <span className={`toggle ${setSpanClassName('my-missions-updates')}`}
-                            onClick={() => handleChange("my-missions-updates")}></span>
+                    <div className={`btn-notefications ${setDivClassName('my-missions-updates')} flex align-center`}
+                        onClick={() => handleChange("my-missions-updates")}>
+                        <span className={`toggle ${setSpanClassName('my-missions-updates')}`}></span>
                     </div>
                 </div>
                 <div className="completed-missions flex space-between align-center">
                     <span>קבלת התראות על משימות שלי שהושלמו</span>
-                    <div className={`btn-notefications ${setDivClassName('completed-missions')} flex align-center`}>
-                        <span className={`toggle ${setSpanClassName('completed-missions')}`}
-                            onClick={() => handleChange("completed-missions")}></span>
+                    <div className={`btn-notefications ${setDivClassName('completed-missions')} flex align-center`}
+                        onClick={() => handleChange("completed-missions")}>
+                        <span className={`toggle ${setSpanClassName('completed-missions')}`}></span>
                     </div>
                 </div>
                 <div className="delayed-missions flex space-between align-center">
                     <span>קבלת התראות על משימות באיחור</span>
-                    <div className={`btn-notefications ${setDivClassName('delayed-missions')} flex align-center`}>
-                        <span className={`toggle ${setSpanClassName('delayed-missions')}`}
-                            onClick={() => handleChange("delayed-missions")}></span>
+                    <div className={`btn-notefications ${setDivClassName('delayed-missions')} flex align-center`}
+                        onClick={() => handleChange("delayed-missions")}>
+                        <span className={`toggle ${setSpanClassName('delayed-missions')}`}></span>
                     </div>
                 </div>
                 <div className="new-comments flex space-between align-center">
                     <span>קבלת התראות על תגובות חדשות</span>
-                    <div className={`btn-notefications ${setDivClassName('new-comments')} flex align-center`}>
-                        <span className={`toggle ${setSpanClassName('new-comments')}`}
-                            onClick={() => handleChange("new-comments")}></span>
+                    <div className={`btn-notefications ${setDivClassName('new-comments')} flex align-center`}
+                        onClick={() => handleChange("new-comments")}>
+                        <span className={`toggle ${setSpanClassName('new-comments')}`}></span>
                     </div>
                 </div>
                 <div className="completed-projects flex space-between align-center">
                     <span>קבלת התראות על פרויקטים שהושלמו</span>
-                    <div className={`btn-notefications ${setDivClassName('completed-projects')} flex align-center`}>
-                        <span className={`toggle ${setSpanClassName('completed-projects')}`}
-                            onClick={() => handleChange("completed-projects")}></span>
+                    <div className={`btn-notefications ${setDivClassName('completed-projects')} flex align-center`}
+                        onClick={() => handleChange("completed-projects")}>
+                        <span className={`toggle ${setSpanClassName('completed-projects')}`}></span>
                     </div>
                 </div>
                 <div className="delayed-projects flex space-between align-center">
                     <span>קבלת התראות על פרויקטים באיחור</span>
-                    <div className={`btn-notefications ${setDivClassName('delayed-projects')} flex align-center`}>
-                        <span className={`toggle ${setSpanClassName('delayed-projects')}`}
-                            onClick={() => handleChange("delayed-projects")}></span>
+                    <div className={`btn-notefications ${setDivClassName('delayed-projects')} flex align-center`}
+                        onClick={() => handleChange("delayed-projects")}>
+                        <span className={`toggle ${setSpanClassName('delayed-projects')}`}></span>
                     </div>
                 </div>
             </div>}
